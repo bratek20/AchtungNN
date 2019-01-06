ad56cfd73595d61fe6e6ce7af505cddc8365567e
Bartosz Rudzki (Home) <bartosz.rudzki@tensquaregames.com>
2019-01-06 14:56:30 +0100

    Test1 implemented.

---
+++ UnityProject/Assets/NN/SingleDotVisual/VisualAgent.cs
@@ -0,0 +1,31 @@
+﻿using MLAgents;
+using UnityEngine;
+using System.Linq;
+
+public class VisualAgent : Agent
+{
+    [SerializeField]
+    private DotHead dot;
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        AddReward(0.01f);
+
+        const float TURN_THRESHOLD = 0.33f;
+        float turnValue = vectorAction[0];
+        if (turnValue < -TURN_THRESHOLD)
+        {
+            dot.TurnLeft();
+        }
+        else if (turnValue > TURN_THRESHOLD)
+        {
+            dot.TurnRight();
+        }
+
+        if (dot.Killed)
+        {
+            Done();
+            SetReward(-1.0f);
+        }
+    }
+}
