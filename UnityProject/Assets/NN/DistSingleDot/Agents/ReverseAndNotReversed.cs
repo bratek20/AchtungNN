059472bc92a050f3404db511918530c2da0f3a32
Krzysztof Bednarek <krzybe@gmail.com>
2019-01-25 11:18:38 +0100

    Distance based observations implementation

---
+++ UnityProject/Assets/NN/DistSingleDot/Agents/ReverseAndNotReversed.cs
@@ -0,0 +1,46 @@
+﻿using MLAgents;
+using UnityEngine;
+using System.Linq;
+
+public class ReverseAndNotReversed : Agent
+{
+    [SerializeField]
+    private int distSamples = 10;
+    [SerializeField]
+    private float angleStep = 5f;
+    [SerializeField]
+    private DotHead dot;
+
+    public override void CollectObservations()
+    {
+        var dists = dot.GetDistsToObstacles(distSamples, angleStep);
+        float max = dists.Max();
+        dists = dists.Select(val => val > 1 ? 1 : val).ToList()
+            .Concat(dists.Select(val => val < 1 ? 1 : 1 / val).ToList()).ToList();
+        AddVectorObs(dists);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        const float TURN_THRESHOLD = 0.33f;
+        float turnValue = vectorAction[0];
+
+        if (turnValue < -TURN_THRESHOLD)
+        {
+            dot.TurnLeft();
+        }
+        else if (turnValue > TURN_THRESHOLD)
+        {
+            dot.TurnRight();
+        }
+
+        if (dot.Killed)
+        {
+            Done();
+        }
+        else
+        {
+            AddReward(0.1f);
+        }
+    }
+}
---
+++ UnityProject/Assets/NN/DistSingleDot/Agents/Sigmoid.cs
@@ -0,0 +1,52 @@
+﻿using MLAgents;
+using UnityEngine;
+using System.Linq;
+
+public class Sigmoid : Agent
+{
+    [SerializeField]
+    private int distSamples = 10;
+    [SerializeField]
+    private float angleStep = 5f;
+    [SerializeField]
+    private DotHead dot;
+
+    public override void CollectObservations()
+    {
+        var dists = dot.GetDistsToObstacles(distSamples, angleStep);
+        float max = dists.Max();
+        dists = dists.Select(val => (float)System.Math.Tanh(val/100)).ToList();
+        /*
+        string tmp = "";
+        foreach (float element in dists)
+            tmp += element.ToString() + " ";
+
+        Debug.Log(tmp);
+        */
+        AddVectorObs(dists);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        const float TURN_THRESHOLD = 0.33f;
+        float turnValue = vectorAction[0];
+
+        if (turnValue < -TURN_THRESHOLD)
+        {
+            dot.TurnLeft();
+        }
+        else if (turnValue > TURN_THRESHOLD)
+        {
+            dot.TurnRight();
+        }
+
+        if (dot.Killed)
+        {
+            Done();
+        }
+        else
+        {
+            AddReward(0.1f);
+        }
+    }
+}
